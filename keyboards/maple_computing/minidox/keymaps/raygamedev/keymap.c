4176a9f40ee0033fa84e00cbbd48c95381da769b
Ray <danr@inspekto.com>
2023-02-12 19:12:15 +0200

    kek

--- keyboards/maple_computing/minidox/keymaps/raygamedev/keymap.c
+++ keyboards/maple_computing/minidox/keymaps/raygamedev/keymap.c
@@ -17,7 +17,8 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
-  RAY
+  RAY,
+  OFFICE
 };
 
 // Defines for task manager and such
@@ -45,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
   LSFT_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  LSFT_T(KC_SLSH),
-                    KC_LCTL, LGUI_T(KC_ESC), LT(LOWER, KC_SPC),         LT(RAISE, KC_BSPC), LT(ADJUST,KC_ENT), OSM(MOD_LSFT)),
+                    KC_LCTL, LGUI_T(KC_ESC), LT(RAISE, KC_SPC),         LT(LOWER, KC_BSPC), LT(ADJUST,KC_ENT), KC_LALT),
 
 /* Raise
  *
@@ -60,12 +61,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                  |      | LOWER|      |    |      | RAISE|      |
  *                  `-------------|      |    |      |------+------.
  *                                |      |    |      |
- *                                `------'    `------'
+ *                               `------'    `------'
  */
 [_RAISE] = LAYOUT_split_3x5_3(
-  LGUI(KC_GRV),    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_RBRC,
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS,  KC_QUOT,
+  LGUI(KC_GRV),    KC_2,    KC_3,    KC_4,    KC_5,         LCA(KC_6),    LCA(KC_7),   LCA(KC_8),    LCA(KC_9),    KC_PGUP,
+  KC_TAB,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_PGDN,
+  KC_LSFT, KC_GRV,  KC_LGUI, KC_LALT, _______,      LCA(KC_H), LCA(KC_J), LCA(KC_K), LCA(KC_L),  KC_QUOT,
                     QK_BOOT, KC_BSPC, _______,      _______, _______, _______
 ),
 
@@ -88,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV, KC_1, KC_2, KC_3,  KC_0,      KC_LBRC, KC_EXLM, KC_AT, KC_RBRC, KC_ASTR,
   KC_TAB,  KC_4, KC_5, KC_6, _______,      KC_LPRN, KC_MINS, KC_EQL, KC_RPRN, KC_QUOT,
   KC_LSFT, KC_7, KC_8, KC_9, _______,      KC_HASH, KC_DLR, KC_AMPR, KC_PIPE,  KC_PERC,
-              KC_0, _______, _______,      KC_BSPC,  _______, KC_DEL
+              KC_0, _______, _______,      KC_BSPC,  _______, KC_BSLS
 ),
 
 /* Adjust (Lower + Raise)
@@ -108,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT_split_3x5_3(
   RAY,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_F11,  KC_F12,  KC_1, _______, _______,      _______, _______, _______, TSKMGR, CALTDEL,
+  OFFICE,  KC_F12,  KC_DEL, _______, _______,      KC_HOME, _______, _______, KC_END, CALTDEL,
   QK_BOOT,   _______, _______, _______, _______,      _______, _______, _______, _______,  _______,
                     _______, _______, _______,      _______,  _______, _______
 )
@@ -158,10 +159,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case OFFICE:
+        if (record->event.pressed) {
+            // when keycode QMKBEST is pressed
+            SEND_STRING("");
+            register_code(KC_ENT);
+            unregister_code(KC_ENT);
+        } else {
+            // when keycode QMKBEST is released
+        }
+        break;
     case RAY:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-            SEND_STRING("lol");
+            SEND_STRING("");
             register_code(KC_ENT);
             unregister_code(KC_ENT);
         } else {
